0e4580520561607de40eae6b140c9bab19ac35dd
Jesús V.G <Jesus777.jsgl@gmail.com>
2021-12-17 21:10:13 -0600

    Seed mejorados, AL crear un nuevo usuario se le asigna en automatico un perfil

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -10,27 +11,55 @@ class UsersSeeder extends Seeder
 {
 	/**
 	 * Run the database seeds.
+	 * use the next command to seed the database
+	 * php artisan db:seed
+	 *
+	 * To clean the database use the next command
+	 * php artisan migrate:refresh
+	 *
+	 * to create the data use the next command
+	 * php artisan db:seed
+	 *
 	 *
 	 * @return void
 	 */
 	public function run()
 	{
 		//
-		DB::table('users')->insert([
-			'name' => 'Juan',
-			'email' => 'juan@gmail.com',
-			'url' => 'https://www.juan.com',
+		$user = User::create([
+			'name' => 'admin',
+			'email' => 'admin@gmail.com',
+			'url' => 'http://localhost:8000/',
 			'password' => Hash::make('12345678'),
-			'created_at' => now(),
-			'updated_at' => now(),
 		]);
-		DB::table('users')->insert([
-			'name' => 'Pedro',
-			'email' => 'pedro@gmail.com',
-			'url' => 'https://www.pedro.com',
+
+		// we create the profile for the user
+		// $user->profile()->create([
+		// 	'imagen' => 'https://source.unsplash.com/random/200x200',
+		// ]);
+
+		$user = User::create([
+			'name' => 'user',
+			'email' => 'user@gmail.com',
+			'url' => 'http://localhost:8000/',
 			'password' => Hash::make('12345678'),
-			'created_at' => now(),
-			'updated_at' => now(),
 		]);
+
+		// we create the profile for the user
+		// $user->profile()->create([
+		// 	'imagen' => 'https://source.unsplash.com/random/200x200',
+		// ]);
+
+		$user = User::create([
+			'name' => 'user2',
+			'email' => 'user2@gmail.com',
+			'url' => 'http://localhost:8000/',
+			'password' => Hash::make('12345678'),
+		]);
+
+		// we create the profile for the user
+		// $user->profile()->create([
+		// 	'imagen' => 'https://source.unsplash.com/random/200x200',
+		// ]);
 	}
 }

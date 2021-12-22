bd2e14d20d2908db3afb03d1a8e2dba595c070dc
Jesús V.G <Jesus777.jsgl@gmail.com>
2021-12-21 20:28:10 -0600

    agregamos un mensaje cuando el usuario aun no tiene recetas creadas

--- resources/views/recetas/index.blade.php
+++ resources/views/recetas/index.blade.php
@@ -8,43 +8,53 @@
 @endsection
 
 @section('content')
-
 	<div class="container">
-		<h1 class="h1 text-center">Administrar tus recetas</h1>
+		<h1 class="h1 text-center">Administra tus recetas</h1>
 
-		<table class="table mt-4 table-striped">
-			<thead class="bg-primary text-white">
-				<tr>
-					<th>Titulo</th>
-					<th>Categoría</th>
-					<th>Acciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($recetas as $receta)
+		{{-- we validate if data recipes not exist --}}
+		@if (count($recetas) > 0)
+			<table class="table mt-4 table-striped">
+				<thead class="bg-primary text-white">
 					<tr>
-						<td scope="row">{{ $receta->titulo }}</td>
-						<td>{{ $receta->categories->nombre }}</td>
-						<td>
-							<eliminar-receta
-								receta-id="{{ $receta->id }}"
-								receta-titulo="{{ $receta->titulo }}"
-							></eliminar-receta>
-
-							<a
-								href="{{ route('recetas.edit', ['receta' => $receta->id]) }}"
-								class="btn btn-primary btn-sm"
-							>Editar</a>
-							<a
-								href="{{ route('recetas.show', ['receta' => $receta->id]) }}"
-								class="btn btn-info btn-sm"
-							>Ver</a>
-						</td>
+						<th>Titulo</th>
+						<th>Categoría</th>
+						<th>Acciones</th>
 					</tr>
-				@endforeach
+				</thead>
+				<tbody>
+					@foreach ($recetas as $receta)
+						<tr>
+							<td scope="row">{{ $receta->titulo }}</td>
+							<td>{{ $receta->categories->nombre }}</td>
+							<td>
+								<eliminar-receta
+									receta-id="{{ $receta->id }}"
+									receta-titulo="{{ $receta->titulo }}"
+								></eliminar-receta>
 
-			</tbody>
-		</table>
+								<a
+									href="{{ route('recetas.edit', ['receta' => $receta->id]) }}"
+									class="btn btn-primary btn-sm"
+								>Editar</a>
+
+								<a
+									href="{{ route('recetas.show', ['receta' => $receta->id]) }}"
+									class="btn btn-info btn-sm"
+								>Ver</a>
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+		@else
+			{{-- we show message if data recipes not exist --}}
+			<div
+				class="alert alert-info mt-4"
+				role="alert"
+			>
+				No tienes recetas creadas
+			</div>
+		@endif
 	</div>
 
 @endsection
